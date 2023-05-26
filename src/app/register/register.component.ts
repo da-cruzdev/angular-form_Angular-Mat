4c7a6074a4a3a9ab92999adb845f0f685e899198
Da-cruz <wilfrieddacruz@arolitec.com>
2023-05-26 09:37:15 +0000

    using formBuilder instead of formControl

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,19 +12,28 @@ export class RegisterComponent implements OnInit {
 
   public registerForm!: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true),
+    this.registerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: false,
     });
   }
 
   public saveData(): void {
     console.log(this.registerForm);
     console.log('Valeurs : ', JSON.stringify(this.registerForm.value));
+  }
+
+  public fillFormData() {
+    this.registerForm.setValue({
+      lastName: ' Da-cruz',
+      firstName: 'Wilfried',
+      email: 'will@dev.com',
+      sendCatalog: true,
+    });
   }
 }
